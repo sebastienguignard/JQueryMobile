2167135d8d6a3ceed3000771e69fe021794b54d1
Sébastien <sebastien-guignard@orange.fr>
2017-02-22 22:58:09 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,63 @@
+$(document).on('pageinit', function(){
+    $.mobile.toolbar.prototype.options.backBtnText = "retour";
+    $.mobile.toolbar.prototype.options.addBackBtn = true;
+});
+
+$(document).on( "pagebeforecreate", function( event ) {
+    console.log("pagebeforecreate avant autre page ");
+} );
+$(document).on( "hashchange", function( event ) {
+    console.log("hashchange");
+} );
+$(document).on( "mobileinit", function( event ) {
+    console.log("mobileinit");
+} );
+$(document).on( "navigate", function( event ) {
+    console.log("navigate");
+} );
+$(document).on( "orientationchange event", function( event ) {
+    console.log("orientationchange event");
+} );
+$(document).on( "pagebeforechange", function( event ) {
+    console.log("pagebeforechange");
+} );
+$(document).on( "pagebeforehide", function( event ) {
+    console.log("pagebeforehide");
+} );
+$(document).on( "pagebeforeload", function( event ) {
+    console.log("pagebeforeload");
+} );
+$(document).on( "pagebeforeshow", function( event ) {
+    console.log("pagebeforeshow");
+
+} );
+$(document).on( "pagechange", function( event ) {
+    console.log("pagechange");
+} );
+$(document).on( "pagebeforecreate", function( event ) {
+    console.log("pagebeforecreate avant autre page ");
+} );
+$(document).on( "pagechangefailed", function( event ) {
+    console.log("pagechangefailed");
+} );
+$(document).on( "pagecreate", function( event ) {
+    console.log("pagecreate");
+} );
+$(document).on( "pagehide", function( event ) {
+    console.log("pagehide");
+} );
+$(document).on( "pageinit", function( event ) {
+    console.log("pageinit");
+} );
+$(document).on( "pageload", function( event ) {
+    console.log("pageload");
+} );
+$(document).on( "pageloadfailed", function( event ) {
+    console.log("pageloadfailed");
+} );
+$(document).on( "pageremove", function( event ) {
+    console.log("pageremove");
+} );
+$(document).on( "pageshow", function( event ) {
+    console.log("pageshow");
+} );
